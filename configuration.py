6bce0740ffbda889b8f5bae0e5a92cee3e0df20b
Анна Иванова <anna12mart@mail.ru>
2023-09-01 11:53:35 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+CREATE_USER_PATH = "/api/v1/users/"
+URL_SERVICE = "https://5f499e53-334c-447c-8d40-1f1b32368e4e.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
